75264831c1641fc61f90110a628f525a3efcbd6e
Dewesh Jha <deweshjha4@gmail.com>
2024-03-02 21:40:21 +0530

    style changes

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,13 @@ import  AddTaskBtn  from './components/TaskAdd/TaskAddButton';
 function App() {
   return (
     <>
-      <div className='app'>
-        <h1 className='header'>ToDew : A simple todo list app</h1>    
+      <div className="app">
+        <h1 className="header">ToDew : A simple todo list app</h1>
         <TaskList></TaskList>
       </div>
       <AddTaskBtn></AddTaskBtn>
-      
     </>
-  )
+  );
 }
 
 export default App;
