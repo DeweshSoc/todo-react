def1647a1942af838c74ce41a28222cc5d38c4a5
Dewesh Jha <deweshjha4@gmail.com>
2024-03-04 17:02:59 +0530

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,50 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { createBrowserRouter, Outlet, Link } from "react-router-dom";
+import "./App.css";
 
-import  TaskList  from './components/TaskList/TaskList';
-import  AddTaskBtn  from './components/TaskAdd/TaskAddButton';
+import { TaskList, AddTaskBtn, About, Error, Task } from "./components";
 
-function App() {
+export function App() {
   return (
     <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/"> Home </Link>
+          </li>
+          <li>
+            <Link to="/about"> About </Link>
+          </li>
+        </ul>
+      </nav>
       <div className="app">
         <h1 className="header">ToDew : A simple todo list app</h1>
-        <TaskList></TaskList>
+        <Outlet />
       </div>
-      <AddTaskBtn></AddTaskBtn>
     </>
   );
 }
+
+export const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path:"",
+        element:<TaskList />
+      },
+      {
+        path:"/task/:taskId",
+        element:<Task/>
+      },
+      {
+        path: "about",
+        element: <About></About>,
+      },
+    ],
+    errorElement: <Error />,
+  },
+]);
 
 export default App;
