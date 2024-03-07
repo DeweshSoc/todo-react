10c5d0162878541ffe733114e762e907b6000245
Dewesh Jha <deweshjha4@gmail.com>
2024-03-07 13:51:58 +0530

    apply tailwind to shimmer ui

--- src/components/Shimmer/ShimmerTaskList.tsx
+++ src/components/Shimmer/ShimmerTaskList.tsx
@@ -3,13 +3,14 @@ import "./Shimmer.css"
 export const ShimmerTaskList = () => {
   return (
     <>
-      <div className="shimmer-task-list">
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+      <div className="mt-12">
+        <ul className="list-none max-w-[510px] w-full my-4 mx-auto">
+          <li className="bg-primary text-primary-text rounded-md py-6 px-4 m-4 shimmer-effect"></li>
+          <li className="bg-primary text-primary-text rounded-md py-6 px-4 m-4 shimmer-effect"></li>
+          <li className="bg-primary text-primary-text rounded-md py-6 px-4 m-4 shimmer-effect"></li>
+          <li className="bg-primary text-primary-text rounded-md py-6 px-4 m-4 shimmer-effect"></li>
+          <li className="bg-primary text-primary-text rounded-md py-6 px-4 m-4 shimmer-effect"></li>
+          <li className="bg-primary text-primary-text rounded-md py-6 px-4 m-4 shimmer-effect"></li>
         </ul>
       </div>
     </>
