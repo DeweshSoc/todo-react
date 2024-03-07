abaa110935e6fc877c3a255fc0cf8b4a6f497f3f
Dewesh Jha <deweshjha4@gmail.com>
2024-03-07 14:26:10 +0530

    add new animation and keyframe

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,21 @@ module.exports = {
         roboto: "Roboto",
         montserrat: "Montserrat",
       },
+      animation:{
+        'task-click':'task-click 1s cubic-bezier(0.075, 0.82, 0.165, 1)'
+      },
       keyframes:{
         'shimmer': {
           'to': {
               'background-position-x': '0%'
+          }
+        },
+        'task-click':{
+          'from':{
+            'transform':'scale(1)'
+          },
+          'to':{
+            'transform':'scale(0.98)'
           }
         }
       }
